31bdaf97565384e91e411a8a2280984acf0cf351
Pedro Volpatto <pedrovolpattocosta@gmail.com>
2024-08-13 20:55:46 -0300

    Consumers

--- src/functional_interface/ConsumerExemplo.java
+++ src/functional_interface/ConsumerExemplo.java
@@ -7,12 +7,10 @@ import java.util.function.Consumer;
 public class ConsumerExemplo {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1,2,3,4,5);
-        Consumer<Integer> imprimirNumeroPar = numero -> {
-            if(numero % 2 == 0){
-                System.out.println(numero);
-            }
-        };
-
-        numeros.stream().forEach(imprimirNumeroPar);
+        numeros.stream().forEach(n -> {
+                if(n % 2 == 0){
+                    System.out.println(n);
+                }
+            });
     }
 }
