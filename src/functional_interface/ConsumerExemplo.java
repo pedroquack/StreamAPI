73a1f71963ab7d584be93677b971498ebd5f5109
Pedro Volpatto <pedrovolpattocosta@gmail.com>
2024-08-13 20:47:42 -0300

    new file:   .vscode/settings.json     new file:   README.md     new file:   bin/App.class     new file:   bin/functional_interface/ConsumerExemplo.class     new file:   src/App.java     new file:   src/functional_interface/ConsumerExemplo.java

---
+++ src/functional_interface/ConsumerExemplo.java
@@ -0,0 +1,18 @@
+package functional_interface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExemplo {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if(numero % 2 == 0){
+                System.out.println(numero);
+            }
+        };
+
+        numeros.stream().forEach(imprimirNumeroPar);
+    }
+}
